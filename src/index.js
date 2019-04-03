54e5e1c597ba62f301deb23d22e66711ed076739
Lampros Papadimitriou <papadimitriou@tyntec.com>
2019-04-03 23:09:25 +0200

    Implement further skeleton steps

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 const createConfig = require('./config');
 const FontelloService = require('./fontello.service');
 const unzip = require('unzip');
+const path = require("path")
 
+/**
+ * Unzips all files by provided stream
+ * @param {object} pipeline
+ * @return {object} containing assets
+ */
 const unzipByPipeline = pipeline => new Promise((resolve, reject) => {
   const assets = {};
 
   pipeline.pipe(unzip.Parse())
     .on('entry', entry => {
-      entry.autodrain();
+      const ext = path.extname(entry.path).slice(1)
+
+      if(entry.type === 'File') {
+        const buffer = [];
+        entry.on("data", data => buffer.push(data))
+        entry.on("end", () => { assets[ext] = Buffer.concat(buffer) })
+      } else {
+        entry.autodrain();
+      }
     })
     .on('error', error => reject(error))
     .on('close', () => resolve(assets));
 });
+
+const getPublicPath = compilation => {
+	let publicPath = compilation.mainTemplate.getPublicPath({ hash: compilation.hash }) || ""
+
+	if(publicPath && publicPath.substr(-1) !== "/") {
+		publicPath += "/"
+	}
+
+	return publicPath
+}
 
 class FontelloWebpackPlugin {
 
@@ -23,13 +47,15 @@ class FontelloWebpackPlugin {
     // specify the hook to attach to
     const fontelloService = new FontelloService(this.options);
 
-    compiler.hooks.emit.tapAsync(
+    compiler.hooks.make.tapAsync(
       'FontelloWebpackPlugin',
       (compilation, callback) => {
         fontelloService.initSession().then(() => {
           fontelloService.fetchFonts().then(body => {
-            unzipByPipeline(body).then(() => {
-              console.log('fonts downloaded and unzipped');
+            unzipByPipeline(body).then(assets => {
+              console.debug(`got assets: ${Object.keys(assets)}`);
+
+              return assets;
             })
           }).catch(error => {
             console.error('Unable to fetch fonts', error);
@@ -37,6 +63,13 @@ class FontelloWebpackPlugin {
         }).catch(error => {
           console.error('session init failed', error);
         });
+        compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing.tapAsync(
+          'FontelloWebpackPlugin',
+          (assets, _callback) => {
+            const publicPath = getPublicPath(compilation);
+            console.log(`publicPath = ${publicPath}`);
+            _callback();
+          });
         callback();
       }
     );
